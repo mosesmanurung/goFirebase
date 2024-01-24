cdc12c0b7cba4ddb7a3724a48c12eb6aa2b3eb3b
mosesmanurung <65177829+mosesmanurung@users.noreply.github.com>
2024-01-24 12:07:11 +0700

    push

--- controllers/firebase.go
+++ controllers/firebase.go
@@ -39,7 +39,8 @@ import (
 	"mime"
 	"net/http"
 	"path/filepath"
-	"strings"
+
+	// "strings"
 
 	// "strings"
 
@@ -47,7 +48,7 @@ import (
 
 	// "strconv"
 
-	"EasyGo/models"
+	// "EasyGo/models"
 
 	"cloud.google.com/go/storage"
 	firebase "firebase.google.com/go/v4"
@@ -137,21 +138,21 @@ func (c *FirebaseFileController) Post() {
 
 	downloadURL := fmt.Sprintf("https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media&token=%s", "fir-file-6a929.appspot.com", newObjectName, token)
 
-	firebaseFile := models.FirebaseFile{
-		FileId:   fileID,
-		FileLink: downloadURL,
-	}
+	// firebaseFile := models.FirebaseFile{
+	// 	FileId:   fileID,
+	// 	FileLink: downloadURL,
+	// }
 
-	fmt.Println(firebaseFile)
+	// fmt.Println(firebaseFile)
 
-	fileId, err := models.AddFirebaseFile(&firebaseFile)
-	if err != nil {
-		log.Fatalln(err)
-		c.CustomAbort(http.StatusInternalServerError, "Error while adding FirebaseFile to the database")
-		return
-	}
+	// fileId, err := models.AddFirebaseFile(&firebaseFile)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// 	c.CustomAbort(http.StatusInternalServerError, "Error while adding FirebaseFile to the database")
+	// 	return
+	// }
 
-	c.Data["json"] = map[string]interface{}{"id": fileId, "file_id": fileID, "file_link": downloadURL}
+	c.Data["json"] = map[string]interface{}{"id": 1, "file_id": fileID, "file_link": downloadURL}
 	c.ServeJSON()
 }
 
@@ -248,68 +249,67 @@ func (c *FirebaseFileController) PostMulti() {
 
 	fmt.Print(pathName)
 
-	firebaseFile := models.FirebaseFile{
-		FileId:   strings.Join(pathName, ","),
-		FileLink: strings.Join(pathFile, ","),
-	}
+	// firebaseFile := models.FirebaseFile{
+	// 	FileId:   strings.Join(pathName, ","),
+	// 	FileLink: strings.Join(pathFile, ","),
+	// }
 
-	fmt.Println(firebaseFile)
+	// fmt.Println(firebaseFile)
 
-	fileId, err := models.AddFirebaseFile(&firebaseFile)
-	if err != nil {
-		log.Fatalln(err)
-		c.CustomAbort(http.StatusInternalServerError, "Error while adding FirebaseFile to the database")
-		return
-	}
+	// fileId, err := models.AddFirebaseFile(&firebaseFile)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// 	c.CustomAbort(http.StatusInternalServerError, "Error while adding FirebaseFile to the database")
+	// 	return
+	// }
 
-	c.Data["json"] = map[string]interface{}{"id": fileId, "file_id": pathFile, "file_link": pathName}
+	c.Data["json"] = map[string]interface{}{"file_id": pathFile, "file_link": pathName}
 	c.ServeJSON()
 }
 
-func (c *FirebaseFileController) PostLink() {
-	fileId := c.GetString("file_id")
-	fileLink := c.GetString("file_link")
+// func (c *FirebaseFileController) GetAll() {
+// 	files, err := models.GetAllFirebaseFiles()
+// 	if err != nil {
+// 		c.CustomAbort(http.StatusInternalServerError, "Error retrieving FirebaseFiles")
+// 		return
+// 	}
 
-	err := models.InsertDataFile(fileLink, fileId)
-	if err != nil {
-		fmt.Print(err)
-		c.Data["json"] = map[string]interface{}{"error": err.Error()}
-	} else {
-		c.Data["json"] = map[string]interface{}{"status": "success"}
-	}
-	c.ServeJSON()
-}
+// 	c.Data["json"] = files
+// 	c.ServeJSON()
+// }
 
-func (c *FirebaseFileController) GetAll() {
-	files, err := models.GetAllFirebaseFiles()
-	if err != nil {
-		c.CustomAbort(http.StatusInternalServerError, "Error retrieving FirebaseFiles")
-		return
-	}
+// func (c *FirebaseFileController) GetOne() {
+// 	id, _ := c.GetInt(":id")
 
-	c.Data["json"] = files
-	c.ServeJSON()
-}
+// 	file, err := models.GetFirebaseFileById(id)
+// 	if err != nil {
+// 		c.CustomAbort(http.StatusNotFound, "FirebaseFile not found")
+// 		return
+// 	}
 
-func (c *FirebaseFileController) GetOne() {
-	id, _ := c.GetInt(":id")
-
-	file, err := models.GetFirebaseFileById(id)
-	if err != nil {
-		c.CustomAbort(http.StatusNotFound, "FirebaseFile not found")
-		return
-	}
-
-	c.Data["json"] = file
-	c.ServeJSON()
-}
+// 	c.Data["json"] = file
+// 	c.ServeJSON()
+// }
 
 // func (c *FirebaseFileController) Put() {
+// 	id, _ := c.GetInt(":id")
+
+// 	// existingFile, err := models.GetFirebaseFileById(id)
+// 	if err != nil {
+// 		c.CustomAbort(http.StatusNotFound, "File not found")
+// 		return
+// 	}
+
+// 	err = deleteFileFromStorage(existingFile.FileId)
+// 	if err != nil {
+// 		c.CustomAbort(http.StatusInternalServerError, "Error deleting file from Firebase Storage")
+// 		return
+// 	}
+
 // 	token := uuid.New().String()
 // 	fileID := token
-// 	id, _ := c.GetInt(":id")
-// 	file, header, err := c.GetFile("file")
 
+// 	file, header, err := c.GetFile("file")
 // 	if err != nil {
 // 		c.CustomAbort(http.StatusBadRequest, "File upload error")
 // 		return
@@ -377,250 +377,148 @@ func (c *FirebaseFileController) GetOne() {
 
 // 	downloadURL := fmt.Sprintf("https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media&token=%s", storageBucket, newObjectName, token)
 
-// 	firebaseFile := models.FirebaseFile{
-// 		Id:       id,
-// 		FileId:   fileID,
-// 		FileLink: downloadURL,
-// 	}
+// 	// firebaseFile := models.FirebaseFile{
+// 	// 	Id:       id,
+// 	// 	FileId:   fileID,
+// 	// 	FileLink: downloadURL,
+// 	// }
 
-// 	if err := models.UpdateFirebaseFile(&firebaseFile); err != nil {
-// 		log.Fatalln(err)
-// 		c.CustomAbort(http.StatusInternalServerError, "Error while updating FirebaseFile in the database")
-// 		return
-// 	}
+// 	// if err := models.UpdateFirebaseFile(&firebaseFile); err != nil {
+// 	// 	log.Fatalln(err)
+// 	// 	c.CustomAbort(http.StatusInternalServerError, "Error while updating FirebaseFile in the database")
+// 	// 	return
+// 	// }
 
 // 	c.Data["json"] = map[string]interface{}{"id": id, "file_id": fileID, "file_link": downloadURL}
 // 	c.ServeJSON()
 // }
 
-func (c *FirebaseFileController) Put() {
-	id, _ := c.GetInt(":id")
+// func (c *FirebaseFileController) PutMulti() {
+// 	id, _ := c.GetInt(":id")
+// 	filePath, _ := web.AppConfig.String("firebase-storage::firebase_cred")
+// 	storageBucket, _ := web.AppConfig.String("firebase-storage::bucket_link")
+// 	pathName := []string{}
+// 	pathFile := []string{}
 
-	existingFile, err := models.GetFirebaseFileById(id)
-	if err != nil {
-		c.CustomAbort(http.StatusNotFound, "File not found")
-		return
-	}
+// 	existingFile, err := models.GetFirebaseFileById(id)
 
-	err = deleteFileFromStorage(existingFile.FileId)
-	if err != nil {
-		c.CustomAbort(http.StatusInternalServerError, "Error deleting file from Firebase Storage")
-		return
-	}
+// 	if err != nil {
+// 		c.CustomAbort(http.StatusNotFound, "File not found")
+// 		return
+// 	}
 
-	token := uuid.New().String()
-	fileID := token
+// 	filesd := strings.Split(existingFile.FileId, ",")
 
-	file, header, err := c.GetFile("file")
-	if err != nil {
-		c.CustomAbort(http.StatusBadRequest, "File upload error")
-		return
-	}
-	defer file.Close()
+// 	for _, temp := range filesd {
+// 		err = deleteFileFromStorage(temp)
+// 		if err != nil {
+// 			c.CustomAbort(http.StatusInternalServerError, "Error deleting file from Firebase Storage")
+// 			return
+// 		}
+// 	}
 
-	filePath, _ := web.AppConfig.String("firebase-storage::firebase_cred")
-	storageBucket, _ := web.AppConfig.String("firebase-storage::bucket_link")
+// 	files, err := c.GetFiles("file")
 
-	opt := option.WithCredentialsFile(filePath)
-	config := &firebase.Config{
-		StorageBucket: storageBucket,
-	}
-	app, err := firebase.NewApp(context.Background(), config, opt)
-	if err != nil {
-		log.Fatalln(err)
-		c.CustomAbort(http.StatusInternalServerError, "Firebase app initialization error")
-		return
-	}
+// 	if err != nil {
+// 		c.CustomAbort(http.StatusBadRequest, "File upload error")
+// 		return
+// 	}
 
-	client, err := app.Storage(context.Background())
-	if err != nil {
-		log.Fatalln(err)
-		c.CustomAbort(http.StatusInternalServerError, "Firebase Storage client initialization error")
-		return
-	}
+// 	for _, fileHeader := range files {
+// 		file, err := fileHeader.Open()
+// 		if err != nil {
+// 			c.CustomAbort(http.StatusBadRequest, "File upload error")
+// 			return
+// 		}
+// 		defer file.Close()
 
-	bucket, err := client.DefaultBucket()
-	if err != nil {
-		log.Fatalln(err)
-		c.CustomAbort(http.StatusInternalServerError, "Firebase default bucket retrieval error")
-		return
-	}
+// 		fileID := uuid.New().String()
+// 		opt := option.WithCredentialsFile(filePath)
+// 		config := &firebase.Config{
+// 			StorageBucket: storageBucket,
+// 		}
+// 		app, err := firebase.NewApp(context.Background(), config, opt)
+// 		if err != nil {
+// 			log.Fatalln(err)
+// 			c.CustomAbort(http.StatusInternalServerError, "Firebase app initialization error")
+// 			return
+// 		}
 
-	newObjectName := fileID
-	newObj := bucket.Object(newObjectName)
-	wc := newObj.NewWriter(context.Background())
+// 		client, err := app.Storage(context.Background())
+// 		if err != nil {
+// 			log.Fatalln(err)
+// 			c.CustomAbort(http.StatusInternalServerError, "Firebase Storage client initialization error")
+// 			return
+// 		}
 
-	if _, err := io.Copy(wc, file); err != nil {
-		log.Fatalln(err)
-		c.CustomAbort(http.StatusInternalServerError, "Error while copying file content to Firebase Storage")
-		return
-	}
+// 		bucket, err := client.DefaultBucket()
+// 		if err != nil {
+// 			log.Fatalln(err)
+// 			c.CustomAbort(http.StatusInternalServerError, "Firebase default bucket retrieval error")
+// 			return
+// 		}
 
-	if err := wc.Close(); err != nil {
-		log.Fatalln(err)
-		c.CustomAbort(http.StatusInternalServerError, "Error while closing Firebase Storage writer")
-		return
-	}
+// 		newObjectName := fileID
+// 		newObj := bucket.Object(newObjectName)
+// 		wc := newObj.NewWriter(context.Background())
 
-	fileType := mime.TypeByExtension(filepath.Ext(header.Filename))
+// 		if _, err := file.Seek(0, 0); err != nil {
+// 			log.Fatalln(err)
+// 			c.CustomAbort(http.StatusInternalServerError, "Error while seeking file content")
+// 			return
+// 		}
+// 		if _, err := io.Copy(wc, file); err != nil {
+// 			log.Fatalln(err)
+// 			c.CustomAbort(http.StatusInternalServerError, "Error while copying file content to Firebase Storage")
+// 			return
+// 		}
+// 		if err := wc.Close(); err != nil {
+// 			log.Fatalln(err)
+// 			c.CustomAbort(http.StatusInternalServerError, "Error while closing Firebase Storage writer")
+// 			return
+// 		}
 
-	objectAttrsToUpdate := storage.ObjectAttrsToUpdate{
-		Metadata: map[string]string{
-			"firebaseStorageDownloadTokens": token,
-			"contentType":                   fileType,
-		},
-	}
+// 		fileType := mime.TypeByExtension(filepath.Ext(fileHeader.Filename))
 
-	if _, err := bucket.Object(newObjectName).Update(context.Background(), objectAttrsToUpdate); err != nil {
-		log.Fatalln(err)
-		c.CustomAbort(http.StatusInternalServerError, "Error while updating object metadata")
-		return
-	}
+// 		fmt.Print(fileType)
 
-	downloadURL := fmt.Sprintf("https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media&token=%s", storageBucket, newObjectName, token)
+// 		objectAttrsToUpdate := storage.ObjectAttrsToUpdate{
+// 			Metadata: map[string]string{
+// 				"firebaseStorageDownloadTokens": fileID,
+// 				"contentType":                   fileType,
+// 			},
+// 		}
 
-	firebaseFile := models.FirebaseFile{
-		Id:       id,
-		FileId:   fileID,
-		FileLink: downloadURL,
-	}
+// 		if _, err := bucket.Object(newObjectName).Update(context.Background(), objectAttrsToUpdate); err != nil {
+// 			log.Fatalln(err)
+// 			c.CustomAbort(http.StatusInternalServerError, "Error while updating object metadata")
+// 			return
+// 		}
 
-	if err := models.UpdateFirebaseFile(&firebaseFile); err != nil {
-		log.Fatalln(err)
-		c.CustomAbort(http.StatusInternalServerError, "Error while updating FirebaseFile in the database")
-		return
-	}
+// 		downloadURL := fmt.Sprintf("https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media&token=%s", "fir-file-6a929.appspot.com", newObjectName, fileID)
+// 		pathFile = append(pathFile, downloadURL)
+// 		pathName = append(pathName, fileID)
+// 	}
 
-	c.Data["json"] = map[string]interface{}{"id": id, "file_id": fileID, "file_link": downloadURL}
-	c.ServeJSON()
-}
+// 	fmt.Print(pathName)
 
-func (c *FirebaseFileController) PutMulti() {
-	id, _ := c.GetInt(":id")
-	filePath, _ := web.AppConfig.String("firebase-storage::firebase_cred")
-	storageBucket, _ := web.AppConfig.String("firebase-storage::bucket_link")
-	pathName := []string{}
-	pathFile := []string{}
+// 	// firebaseFile := models.FirebaseFile{
+// 	// 	FileId:   strings.Join(pathName, ","),
+// 	// 	FileLink: strings.Join(pathFile, ","),
+// 	// }
 
-	existingFile, err := models.GetFirebaseFileById(id)
+// 	// fmt.Println(firebaseFile)
 
-	if err != nil {
-		c.CustomAbort(http.StatusNotFound, "File not found")
-		return
-	}
+// 	// fileId, err := models.AddFirebaseFile(&firebaseFile)
+// 	// if err != nil {
+// 	// 	log.Fatalln(err)
+// 	// 	c.CustomAbort(http.StatusInternalServerError, "Error while adding FirebaseFile to the database")
+// 	// 	return
+// 	// }
 
-	filesd := strings.Split(existingFile.FileId, ",")
-
-	for _, temp := range filesd {
-		err = deleteFileFromStorage(temp)
-		if err != nil {
-			c.CustomAbort(http.StatusInternalServerError, "Error deleting file from Firebase Storage")
-			return
-		}
-	}
-
-	files, err := c.GetFiles("file")
-
-	if err != nil {
-		c.CustomAbort(http.StatusBadRequest, "File upload error")
-		return
-	}
-
-	for _, fileHeader := range files {
-		file, err := fileHeader.Open()
-		if err != nil {
-			c.CustomAbort(http.StatusBadRequest, "File upload error")
-			return
-		}
-		defer file.Close()
-
-		fileID := uuid.New().String()
-		opt := option.WithCredentialsFile(filePath)
-		config := &firebase.Config{
-			StorageBucket: storageBucket,
-		}
-		app, err := firebase.NewApp(context.Background(), config, opt)
-		if err != nil {
-			log.Fatalln(err)
-			c.CustomAbort(http.StatusInternalServerError, "Firebase app initialization error")
-			return
-		}
-
-		client, err := app.Storage(context.Background())
-		if err != nil {
-			log.Fatalln(err)
-			c.CustomAbort(http.StatusInternalServerError, "Firebase Storage client initialization error")
-			return
-		}
-
-		bucket, err := client.DefaultBucket()
-		if err != nil {
-			log.Fatalln(err)
-			c.CustomAbort(http.StatusInternalServerError, "Firebase default bucket retrieval error")
-			return
-		}
-
-		newObjectName := fileID
-		newObj := bucket.Object(newObjectName)
-		wc := newObj.NewWriter(context.Background())
-
-		if _, err := file.Seek(0, 0); err != nil {
-			log.Fatalln(err)
-			c.CustomAbort(http.StatusInternalServerError, "Error while seeking file content")
-			return
-		}
-		if _, err := io.Copy(wc, file); err != nil {
-			log.Fatalln(err)
-			c.CustomAbort(http.StatusInternalServerError, "Error while copying file content to Firebase Storage")
-			return
-		}
-		if err := wc.Close(); err != nil {
-			log.Fatalln(err)
-			c.CustomAbort(http.StatusInternalServerError, "Error while closing Firebase Storage writer")
-			return
-		}
-
-		fileType := mime.TypeByExtension(filepath.Ext(fileHeader.Filename))
-
-		fmt.Print(fileType)
-
-		objectAttrsToUpdate := storage.ObjectAttrsToUpdate{
-			Metadata: map[string]string{
-				"firebaseStorageDownloadTokens": fileID,
-				"contentType":                   fileType,
-			},
-		}
-
-		if _, err := bucket.Object(newObjectName).Update(context.Background(), objectAttrsToUpdate); err != nil {
-			log.Fatalln(err)
-			c.CustomAbort(http.StatusInternalServerError, "Error while updating object metadata")
-			return
-		}
-
-		downloadURL := fmt.Sprintf("https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media&token=%s", "fir-file-6a929.appspot.com", newObjectName, fileID)
-		pathFile = append(pathFile, downloadURL)
-		pathName = append(pathName, fileID)
-	}
-
-	fmt.Print(pathName)
-
-	firebaseFile := models.FirebaseFile{
-		FileId:   strings.Join(pathName, ","),
-		FileLink: strings.Join(pathFile, ","),
-	}
-
-	fmt.Println(firebaseFile)
-
-	fileId, err := models.AddFirebaseFile(&firebaseFile)
-	if err != nil {
-		log.Fatalln(err)
-		c.CustomAbort(http.StatusInternalServerError, "Error while adding FirebaseFile to the database")
-		return
-	}
-
-	c.Data["json"] = map[string]interface{}{"id": fileId, "file_id": pathFile, "file_link": pathName}
-	c.ServeJSON()
-}
+// 	c.Data["json"] = map[string]interface{}{"file_id": pathFile, "file_link": pathName}
+// 	c.ServeJSON()
+// }
 
 func deleteFileFromStorage(fileID string) error {
 	filePath, _ := web.AppConfig.String("firebase-storage::firebase_cred")
--- routers/router.go
+++ routers/router.go
@@ -10,9 +10,6 @@ func init() {
 	web.Router("/", &controllers.MainController{})
 	web.Router("/report/mileage", &controllers.ReportController{}, "get:Mileage")
 	web.Router("/report/mileage/multiple", &controllers.ReportController{}, "get:MultipleMileage")
-	web.Router("/firebasefile", &controllers.FirebaseFileController{}, "post:Post;get:GetAll")
-	web.Router("/firebasefile/multiple", &controllers.FirebaseFileController{}, "post:PostMulti;get:GetAll")
-	web.Router("/firebasefile/multiple/:id", &controllers.FirebaseFileController{}, "put:PutMulti;get:GetAll")
-	web.Router("/firebasefile/:id", &controllers.FirebaseFileController{}, "get:GetOne;put:Put")
-	web.Router("/firebaselink", &controllers.FirebaseFileController{}, "post:PostLink")
+	web.Router("/firebasefile", &controllers.FirebaseFileController{}, "post:Post")
+	web.Router("/firebasefile/multiple", &controllers.FirebaseFileController{}, "post:PostMulti")
 }
