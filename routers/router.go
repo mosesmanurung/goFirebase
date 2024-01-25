9223bc708cc1f5a7c778c49796ce248ea9c6ee0b
mosesmanurung <65177829+mosesmanurung@users.noreply.github.com>
2024-01-25 11:24:56 +0700

    push

--- routers/router.go
+++ routers/router.go
@@ -11,5 +11,5 @@ func init() {
 	web.Router("/report/mileage", &controllers.ReportController{}, "get:Mileage")
 	web.Router("/report/mileage/multiple", &controllers.ReportController{}, "get:MultipleMileage")
 	web.Router("/firebasefile", &controllers.FirebaseFileController{}, "post:Post")
-	web.Router("/firebasefile/multiple", &controllers.FirebaseFileController{}, "post:PostMulti")
+	// web.Router("/firebasefile/multiple", &controllers.FirebaseFileController{}, "post:PostMulti")
 }
